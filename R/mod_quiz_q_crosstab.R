353cb619d3407323c1e3468e23dde6bb1191a83b
Miguel de Iruarrizaga <mdeiruar@gmail.com>
2023-11-10 10:40:37 -0500

    cleaned the new tab, still working wit issues to show a table or options in the questions

--- R/mod_quiz_q_crosstab.R
+++ R/mod_quiz_q_crosstab.R
@@ -15,35 +15,30 @@ mod_quiz_crosstab_ui <- function(id) {
       bs4Dash::column(
         width = 4,
         shiny::selectInput(
-          ns("crosstab_question"),
-          "Question",
+          ns("crosstab_question1"),
+          "Question 1",
           choices = c(""),
           selected = NULL
         ),
-        shiny::textInput(
-          ns("crosstab_correctanswer"),
-          label = "Enter a reference point (if one exists)",
-          value = "None"
-        ),
-       shinyWidgets::prettySwitch(
-          inputId = ns("crosstab_axis_XRange"),
-          label = "X axis: set range 0-100",
-          status = "info",
-          fill = TRUE,
-          value = TRUE
-        )
+        shiny::selectInput(
+          ns("crosstab_question2"),
+          "Question 2",
+          choices = c(""),
+          selected = NULL
       ),
+            ),
       bs4Dash::column(
         width = 8,
         shinycssloaders::withSpinner(
-          highcharter::highchartOutput(
-            outputId = ns("crosstab_graph")
+          DT::dataTableOutput(
+            outputId = ns("crosstab_table")
           )
         )
       )
     )
   )
 }
+
 
 #' mod_crosstab_questionviz Server Functions
 #'
@@ -61,25 +56,23 @@ mod_quiz_crosstab_server <- function(id, stringAsFactors = FALSE, main_inputs, q
         NULL
       })
     })
-
+    #
+    # #one block of code no error so far
     answers <- shiny::reactive({
       tryCatch({
         answers_temp <- quiz_processed() %>%
-          mutate(!!input$crosstab_question := as.character(.data[[input$crosstab_question]])) %>%
-          mutate(!!input$crosstab_question := case_when(
-            is.na(.data[[input$crosstab_question]]) ~ "No answer",
-            TRUE ~ .data[[input$crosstab_question]],
+          mutate(!!input$crosstab_question1 := as.character(.data[[input$crosstab_question1]])) %>%
+          mutate(!!input$crosstab_question1 := case_when(
+            is.na(.data[[input$crosstab_question1]]) ~ "No answer",
+            TRUE ~ .data[[input$crosstab_question1]],
           )) %>%
-          group_by(.data[[input$crosstab_question]]) %>%
+          group_by(.data[[input$crosstab_question1]]) %>%
           summarize(n = n()) %>%
           ungroup()
-        if(input$crosstab_arrange_by_frequency){
-          answers_temp <- answers_temp |>
-            arrange(desc(n))
-        }
+
 
         answers_temp <- answers_temp |>
-          pull(.data[[input$crosstab_question]])
+          pull(.data[[input$crosstab_question1]])
 
         c("No correct answer", answers_temp)
       }, error= function(e){
@@ -87,78 +80,55 @@ mod_quiz_crosstab_server <- function(id, stringAsFactors = FALSE, main_inputs, q
       })
     })
 
-    shiny::observeEvent(quiz_processed(), {
-      shiny::updateSelectInput(
-        session,
-        inputId = "crosstab_question",
-        choices = questions(),
-        selected = questions()[1]
-      )
-    })
+    #second block of code / problem could not find function "quiz_processed"
 
-    output$crosstab_graph <- highcharter::renderHighchart({
-      shiny::req(quiz_processed())
-      shiny::validate(
-        shiny::need(
-          !is.null(quiz_processed()),
-          message = "Quiz was not uploaded correctly. Check your inputs."
-        )
-      )
-      tryCatch({
-        quiz = quiz_processed()
-        question = input$crosstab_question
-        correct_answer = input$crosstab_correctanswer
-        axispercentage = input$crosstab_axis_percentage
-        axisXRange = input$crosstab_axis_XRange
-        question_title <- ifelse(nchar(question) > 200,
-                                 paste0(substr(question, 1, 200), "..."),
-                                 question)
+# shiny::observeEvent(quiz_processed(), {
+#   shiny::updateSelectInput(
+#     session,
+#     inputId = "crosstab_question1",
+#     choices = questions(),
+#     selected = questions()[1]
+#   )
+# })
 
-        # Code for predictions without percentages
+    # shiny::observeEvent(quiz_processed(), {
+    #   shiny::updateSelectInput(
+    #     session,
+    #     inputId = "crosstab_question1",
+    #     choices = questions(),
+    #     selected = questions()[1]
+    #   )
+    # })
 
-          if (!is.na(correct_answer)) {
-            quiz %>%
-              pull(question) %>%
-              unlist() %>%
-              as.numeric() %>%
-              hchart() %>%
-              hc_legend(enabled = FALSE) %>%
-              hc_yAxis(title = list(text = "Number of respondents")) %>%
-              hc_xAxis(title = list(text = "Probability")) %>%
-              hc_xAxis(labels = list(style = list(fontSize = "13px")) ) %>%
-              hc_yAxis(labels = list(style = list(fontSize = "13px")) ) %>%
-              hc_title(text = question_title, style = list(fontSize = "15px"))  %>%
-              hc_xAxis(plotLines = list(
-                list(
-                  color = if (correct_answer != "") "#FF5733" else "#FFFFFF",
-                  dashStyle = "Solid",
-                  width = if (correct_answer != "") 3 else 0,
-                  value = correct_answer,
-                  zIndex = 10
-                )
-              )) %>%
-              hc_xAxis(
-                min = if (axisXRange) 0 else NULL, # Set min to 0 if axisXRange is TRUE
-                max = if (axisXRange) 100 else NULL
-              ) %>%
-              hc_exporting(
-                enabled = TRUE, # always enabled
-                filename = paste0("viz_", substr(question, 1, 20))
-              ) %>%
-              hc_chart(
-                backgroundColor = "#FFFFFF"
-              ) # Set the background color to white
-          } else {
-            quiz %>%
-              pull(question) %>%
-              unlist() %>%
-              as.numeric() %>%
-              hchart() %>%
-              hc_legend(enabled = FALSE)
-        }
-      }, error = function(e){
-        NULL
-      })
-    })
+    # shiny::observeEvent(quiz_processed(), {
+    #   shiny::updateSelectInput(
+    #     session,
+    #     inputId = "crosstab_question2",
+    #     choices = questions(),
+    #     selected = questions()[1]
+    #   )
+    # })
+
+    #Third block argument "name" is missing, with no default
+
+    # output$crosstab_table <- DT::renderDataTable({
+    #   shiny::req(quiz_processed())
+    #   shiny::validate(
+    #     shiny::need(
+    #       !is.null(quiz_processed()),
+    #       message = "Quiz was not uploaded correctly. Check your inputs."
+    #     )
+    #   )
+    #   tryCatch({
+    #     question_data <- answers()
+    #     datatable(question_data, options = list(pageLength = 10))  # Adjust options as needed
+    #   }, error = function(e){
+    #     DT::datatable(data.frame())  # Return an empty table in case of an error
+    #
+    #
+    #   }, error = function(e){
+    #     NULL
+    #   })
+    # })
   })
 }
